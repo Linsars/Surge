4afb0f61dea7ba51bc047a5835be68c6906bb9b6
Linsars <1548943804@qq.com>
2026-04-24 01:38:43 +0800

    Update egern-twxz.js

--- JS/egern-twxz.js
+++ JS/egern-twxz.js
@@ -10,8 +10,8 @@ VIP = switch, false, tag = [启用]会员, desc = 关闭开关将不对此选项
 Video = switch, false, tag = [视频-付费], desc = 关闭开关将不对此选项生效
 
 [Script]
-http-response ^https:\/\/admin\.tanweixianzhi\.com\/prod\/api\/centre\/userInfo script-path=https://he2o.vercel.app/Resource/Plugin/Tweixz.js, requires-body=true, timeout=60, tag=会员, enable={VIP}
-http-response ^https:\/\/admin\.tanweixianzhi\.com\/prod\/api\/video\/info script-path=https://he2o.vercel.app/Resource/Plugin/Tweixz.js, requires-body=true, timeout=60, tag=付费, enable={Video}
+http-response ^https:\/\/admin\.tanweixianzhi\.com\/prod\/api\/centre\/userInfo script-path=https://raw.githubusercontent.com/Linsars/Surge/main/JS/egern-twxz.js, requires-body=true, timeout=60, tag=会员, enable={VIP}
+http-response ^https:\/\/admin\.tanweixianzhi\.com\/prod\/api\/video\/info script-path=https://raw.githubusercontent.com/Linsars/Surge/main/JS/egern-twxz.js, requires-body=true, timeout=60, tag=付费, enable={Video}
 
 [Mitm]
 hostname= admin.tanweixianzhi.com
