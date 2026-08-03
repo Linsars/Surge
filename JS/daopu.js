f368857701ec91aa42d1872a1e5d48207c0b2c0f
Linsars <1548943804@qq.com>
2026-08-03 21:37:07 +0800

    Create daopu.js

---
+++ JS/daopu.js
@@ -0,0 +1,18 @@
+let body = $response.body;
+try {
+  let obj = JSON.parse(body);
+  if (obj && typeof obj === 'object') {
+    obj.activated = true;
+    obj.activationRequired = false;
+    if (obj.authorizationExpiresAt === null || obj.authorizationExpiresAt === undefined) {
+      // 授权有效期设到 2099 年；app 若要求毫秒时间戳，改成 4092595200000
+      obj.authorizationExpiresAt = '2099-12-31T15:59:59Z';
+    }
+    $done({ body: JSON.stringify(obj) });
+  } else {
+    $done({});
+  }
+} catch (e) {
+  // 非 JSON 或解析失败，原样放行
+  $done({});
+}
